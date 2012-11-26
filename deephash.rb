0c6f047aad6d94d991f2998679e53321672d6861
Peter Marreck <lumbergh@gmail.com>
2012-11-26 09:09:15 -0500

    A simple way to do autovivification of hash keys to any depth.

---
+++ deephash.rb
@@ -0,0 +1,20 @@
+# Autovivication
+
+# deep = Hash.new { |hash, key| hash[key] = Hash.new(&hash.default_proc) }
+
+class Deephash < Hash
+  def initialize
+    super { |hash, key| hash[key] = Hash.new(&hash.default_proc) }
+  end
+end
+
+if __FILE__==$PROGRAM_NAME
+  require 'test/unit'
+  class TestDeephash < Test::Unit::TestCase
+    def test_assignment
+      dh = Deephash.new
+      dh[:a][:b][:c] = 5
+      assert_equal 5, dh[:a][:b][:c]
+    end
+  end
+end

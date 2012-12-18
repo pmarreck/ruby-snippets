89a9b104b410198dfcc3a5f79ba6c521e9d03149
Peter Marreck <lumbergh@gmail.com>
2012-12-18 16:03:01 -0500

    added in a way to declare dependencies on namespaced classes

--- abstract_class_dependency.rb
+++ abstract_class_dependency.rb
@@ -8,18 +8,35 @@ module AbstractClassDependency
   module ClassMethods
     def depends_on_class(class_str, params={})
       raise(ArgumentError, 'First parameter must not be an actual Class') if class_str.is_a? Class
-      class_str = (class_sym = class_str.to_sym).to_s
-      class_name_meth = params && params[:as]
-      class_name_meth ||= (class_str.underscore << '_class').to_sym
+      class_str = class_str.to_s
+      class_name_meth = (params && params[:as] && params[:as].to_s) || (class_str.underscore << '_class')
+      class_ivar_name = class_name_meth.gsub(/\//,'__')
+      class_name_meth = class_name_meth.to_sym
       define_singleton_method(class_name_meth) do
-        if (klass = instance_variable_get("@#{class_name_meth}".to_sym))
+        if (klass = instance_variable_get("@#{class_ivar_name}".to_sym))
           klass
         else
-          instance_variable_set("@#{class_name_meth}".to_sym, const_get(class_sym))
+          instance_variable_set("@#{class_ivar_name}".to_sym, begin
+            # handle namespacing
+            namespaced = class_str.split('::')
+            namespaced.inject(Module){|m,c| m.const_get(c) }
+          end)
         end
       end
       define_method(class_name_meth) do
         self.class.send(class_name_meth)
+      end
+    end
+    def depends_on_classes(*args)
+      args = args.flatten
+      if (h=args.first).is_a? Hash
+        h.each do |k,v|
+          depends_on_class k, as: v
+        end
+      else
+        args.each do |c|
+          depends_on_class c
+        end
       end
     end
   end
@@ -34,6 +51,13 @@ if __FILE__==$PROGRAM_NAME
   class C
     def self.ho
       'ho'
+    end
+  end
+  module Yo
+    class MTV
+      def self.raps
+        'Run DMC'
+      end
     end
   end
   class A
@@ -53,6 +77,37 @@ if __FILE__==$PROGRAM_NAME
       see_klass.ho
     end
   end
+  class D
+    include AbstractClassDependency
+    depends_on_classes B: :b_class, C: :see_class
+    def check_b
+      b_class.hi
+    end
+    def self.check_c
+      see_class.ho
+    end
+  end
+  class E
+    include AbstractClassDependency
+    depends_on_classes :B, :C
+    def check_b
+      b_class.hi
+    end
+    def self.check_c
+      c_class.ho
+    end
+  end
+  class F
+    include AbstractClassDependency
+    depends_on_class :'Yo::MTV', as: :yo_mtv
+    depends_on_class :'Yo::MTV'
+    def self.check_run_named
+      yo_mtv.raps
+    end
+    def self.check_run
+      send('yo/mtv_class').raps
+    end
+  end
 
   require 'test/unit'
   class TestAbstractDependency < Test::Unit::TestCase
@@ -66,6 +121,16 @@ if __FILE__==$PROGRAM_NAME
     end
     def test_depends_on_class_with_bad_first_parameter
       assert_raise(ArgumentError){ A.depends_on_class C }
+    end
+    def test_depends_on_classes
+      assert_equal 'hi', D.new.check_b
+      assert_equal 'ho', D.check_c
+      assert_equal 'hi', E.new.check_b
+      assert_equal 'ho', E.check_c
+    end
+    def test_depends_on_namespaced_classes
+      assert_equal 'Run DMC', F.check_run_named
+      assert_equal 'Run DMC', F.check_run
     end
   end
 end

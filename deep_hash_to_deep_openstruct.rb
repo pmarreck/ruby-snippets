cf0eaba915e260548b7b2f150baf9b0663c6bd51
Peter Marreck <lumbergh@gmail.com>
2013-04-09 18:54:44 -0400

    A little util method to convert deep hashes to deep openstructs so they can be navigated via methods.

---
+++ deep_hash_to_deep_openstruct.rb
@@ -0,0 +1,30 @@
+require 'ostruct'
+module HashToOpenstruct
+  def to_ostruct
+    o = OpenStruct.new(self)
+    each.with_object(o) do |(k,v), o|
+      o.send(:"#{k}=", v.to_ostruct) if v.respond_to? :to_ostruct
+    end
+    o
+  end
+end
+
+########## inline tests
+if __FILE__==$PROGRAM_NAME
+  require 'test/unit'
+  Hash.send(:include, HashToOpenstruct)
+  class HashToOpenstructTest < Test::Unit::TestCase
+    def setup
+      @h = {a: {b: {'c' => 5}}}
+    end
+    def test_to_openstruct
+      assert_equal 5, @h.to_ostruct.a.b.c
+    end
+    def test_object_generation_profile
+      os=0; ObjectSpace.each_object(OpenStruct){|o| os+=1 }
+      v = @h.to_ostruct.a.b.c
+      osa=0; ObjectSpace.each_object(OpenStruct){|o| osa+=1 }
+      assert_equal 3, osa - os
+    end
+  end
+end

bc0abcf98bc829e02990c024f4a9a9851e89d3a4
Peter Marreck <lumbergh@gmail.com>
2013-05-07 16:50:48 -0400

    My own ultra simple test class, and allowing asserts everywhere.

---
+++ assertions_and_tests_everywhere.rb
@@ -0,0 +1,73 @@
+module Kernel
+  AssertionError = Class.new(RuntimeError)
+  def assert_equal(*args)
+    if defined?(Test::TRACK)
+      Test::TRACK[:asserts] ||= 0
+      Test::TRACK[:asserts] += 1
+    end
+    expected, got = args[0], args[1]
+    unless expected==got
+      msg = "<#{expected}> expected but was\n<#{got}>."
+      msg << ": #{args[2]}" if args[2]
+      raise AssertionError, msg
+    end
+  end
+  def assert(bool, txt=nil)
+    if defined?(Test::TRACK)
+      Test::TRACK[:asserts] ||= 0
+      Test::TRACK[:asserts] += 1
+    end
+    unless bool
+      msg = "Expected #{bool} to be true"
+      msg << ": #{txt}" if txt
+      raise AssertionError, msg
+    end
+  end
+end
+
+class Test
+  TRACK = {}
+  def self.inherited(base)
+    if TRACK[:tests].nil?
+      at_exit do
+        puts; puts
+        if TRACK[:failures].nil? && TRACK[:errors].nil?
+          puts "Passed"
+        else
+          TRACK[:failures].each{ |e| puts e } if TRACK[:failures]
+          TRACK[:errors].each{ |e| puts e } if TRACK[:errors]
+        end
+        tot_t = Time.now - TRACK[:start_time]
+        puts
+        puts "Finished tests in #{tot_t}s, #{TRACK[:tests].to_f/tot_t} tests/s, #{TRACK[:asserts].to_f/tot_t} assertions/s."
+        puts
+        puts "#{TRACK[:tests]} tests, #{TRACK[:asserts]} assertions, #{TRACK[:failures] ? TRACK[:failures].length : 0} failures, #{TRACK[:errors] ? TRACK[:errors].length : 0} errors"
+      end
+    end
+    at_exit do
+      TRACK[:tests] ||= 0
+      TRACK[:tests] += 1
+      t = base.new
+      base.instance_methods(false).select{|m| m.to_s =~ /^test_/}.each do |m|
+        pass = false
+        begin
+          pass = t.send m
+          print '.' if pass
+        rescue AssertionError => e
+          pass = false
+          TRACK[:failures] ||= []
+          TRACK[:failures] << e.message
+        rescue => e
+          pass = false
+          TRACK[:errors] ||= []
+          TRACK[:errors] << e.message
+        end
+        print 'F' unless pass
+      end
+    end
+    at_exit do
+      puts; puts "# Running tests:"; puts
+      TRACK[:start_time] = Time.now
+    end
+  end
+end
--- ruby_runtime_profiling.rb
+++ ruby_runtime_profiling.rb
@@ -1,6 +1,7 @@
-module Kernel
-
-  def profile_memory
+module ResourceConsumptionProfiler
+  extend self
+  def profile_resource_consumption(params = {})
+    require 'benchmark' unless defined?(Benchmark) if params[:benchmark]
     # First thing to do is track the observer effect so we don't trip on ourselves when measuring.
     # This is trickier than you think- always run the test after ANY code change to make sure
     # you didn't inadvertently create more objects!
@@ -22,13 +23,36 @@ module Kernel
       object_snapshot[o.class] << o.object_id unless (Array===o || Hash===o) && heisenbergs.include?(o.object_id)
     end
 
+    gc_free = ObjectSpace.count_objects[:FREE]
+    params[:stress] ? GC.stress=true : GC.disable
     start_time = Time.now
     heisenbergs << start_time.object_id
+    out = {}
+    heisenbergs << out.object_id
+    size_extra = {}
+    heisenbergs << size_extra.object_id
 
-    yield
+
+    if params[:benchmark]
+      bmt = Benchmark.measure{ yield }
+    else
+      yield
+    end
+
+    if params[:benchmark]
+      heisenbergs << bmt.object_id
+      bmiv = bmt.instance_variables
+      heisenbergs << bmiv.object_id
+      bmiv.each{|iv| heisenbergs << bmt.instance_variable_get(iv).object_id }
+    end
 
     end_time = Time.now
     heisenbergs << end_time.object_id
+    params[:stress] ? GC.stress=false : GC.enable
+    osco = {}
+    heisenbergs << osco.object_id
+    free_loss = gc_free - ObjectSpace.count_objects(osco)[:FREE]
+    GC.start
 
     object_snapshot_post[Symbol] = (sym_ary = Symbol.all_symbols).map{|s| s.object_id}
     heisenbergs << object_snapshot_post[Symbol].object_id
@@ -42,12 +66,20 @@ module Kernel
       object_snapshot_post[o.class] << o.object_id unless (Time===o || Array===o || Hash===o) && heisenbergs.include?(o.object_id)
     end
 
-    out = {}
-    size_extra = {}
     osp_symbol = 0
     object_snapshot_post.each do |klass, oids|
       klass_diff = oids - (object_snapshot[klass] || [])
       out[klass] = klass_diff.length
+      if params[:output_objects] && out[klass] > 0
+        if TrueClass===params[:output_objects] ||
+          (Array===params[:output_objects] && params[:output_objects].include?(klass)) ||
+          (Class===params[:output_objects] && params[:output_objects] == klass)
+          puts "New objects of class #{klass}:"
+          klass_diff.each do |oid|
+            puts "  #{ObjectSpace._id2ref(oid).inspect}"
+          end
+        end
+      end
       out.select!{|k,v| v > 0}
       if klass_diff.size > 0 && ObjectSpace._id2ref(klass_diff.first).respond_to?(:size)
         size_extra[klass] = klass_diff.map{|oid| ObjectSpace._id2ref(oid)}.inject(0) do |s, o|
@@ -59,6 +91,8 @@ module Kernel
         end
       end
     end
+    size_extra[:free_mem_bytes_consumed] = free_loss
+    size_extra[:user_cpu_time] = bmt.utime if params[:benchmark]
     object_snapshot.clear
     object_snapshot_post.clear
     [out, size_extra, end_time - start_time]
@@ -67,25 +101,51 @@ end
 
 ########## inline tests
 if __FILE__==$PROGRAM_NAME
+  class Object
+    include ResourceConsumptionProfiler
+  end
+
   require 'test/unit'
   class RuntimeProfilingTest < Test::Unit::TestCase
+
+    def test_expected_measurements_for_empty_block
+      num_objects, sizes, time = profile_resource_consumption {}
+      assert_equal({}, num_objects)
+      assert_equal({:free_mem_bytes_consumed=>6}, sizes)
+    end
+
     def test_expected_measurements
-      num_objects, sizes, time = profile_memory do
-        [] << "hi there"
+      a = []
+      num_objects, sizes, time = profile_resource_consumption do
+        s = "hi there"
+        n = "nope"
+        ar = []
+        ar << s
+        a << ar
         b = "crazy"
         h = {a: 'b'}
+        a << h
         t = Time.now
+        a << t
         s = :hellotherebaby
         g = b.to_sym # note that this is the only one that counts as "new"
         10.times { "unbelievable" }
       end
-      assert_equal({Symbol=>1, String=>14, Array=>1, Time=>1, Hash=>1}, num_objects)
-      assert_equal({Symbol=>5, String=>139, Array=>1, Hash=>1}, sizes)
+      assert_equal({Symbol=>1, String=>3, Array=>1, Time=>1, Hash=>1}, num_objects)
+      assert_equal({Symbol=>5, String=>14, Array=>1, Hash=>1, :free_mem_bytes_consumed=>24}, sizes)
       assert time > 0
       assert time < 1
     end
+
+    def test_expected_with_benchmark_empty_block
+      num_objects, sizes, time = profile_resource_consumption(benchmark: true) {}
+      # Note that benchmarking adds a few extra objects. It is too much of a pain to tease them out at this time.
+      assert_equal({String=>1, Float=>6, Benchmark::Tms=>1}, num_objects)
+      assert_equal({String=>0, :free_mem_bytes_consumed=>39, :user_cpu_time=>0.0}, sizes)
+    end
+
     # just to demonstrate...
-    p profile_memory { require 'active_support' }
+    p profile_resource_consumption { require 'active_support' }
   end
 end
 

d3f290f417c1e2ae848eb9928446362eaec6630d
Peter Marreck <lumbergh@gmail.com>
2013-07-30 11:02:06 -0400

    Formatting tweaks and additional "core monkeypatch mode" test to BaseN

--- base_anything.rb
+++ base_anything.rb
@@ -3,67 +3,71 @@ class BaseN
   PGP_EVEN_WORDS = []
   PGP_ODD_WORDS = []
   PGP_WORDS = [PGP_EVEN_WORDS, PGP_ODD_WORDS]
-  ["aardvark", "adroitness", "absurd", "adviser", "accrue", "aftermath", "acme", "aggregate", "adrift", "alkali",
-    "adult", "almighty", "afflict", "amulet", "ahead", "amusement", "aimless", "antenna", "Algol", "applicant",
-    "allow", "Apollo", "alone", "armistice", "ammo", "article", "ancient", "asteroid", "apple", "Atlantic",
-    "artist", "atmosphere", "assume", "autopsy", "Athens", "Babylon", "atlas", "backwater", "Aztec", "barbecue",
-    "baboon", "belowground", "backfield", "bifocals", "backward", "bodyguard", "banjo", "bookseller",
-    "beaming", "borderline", "bedlamp", "bottomless", "beehive", "Bradbury", "beeswax", "bravado",
-    "befriend", "Brazilian", "Belfast", "breakaway", "berserk", "Burlington", "billiard", "businessman",
-    "bison", "butterfat", "blackjack", "Camelot", "blockade", "candidate", "blowtorch", "cannonball",
-    "bluebird", "Capricorn", "bombast", "caravan", "bookshelf", "caretaker", "brackish", "celebrate",
-    "breadline", "cellulose", "breakup", "certify", "brickyard", "chambermaid", "briefcase", "Cherokee",
-    "Burbank", "Chicago", "button", "clergyman", "buzzard", "coherence", "cement", "combustion",
-    "chairlift", "commando", "chatter", "company", "checkup", "component", "chisel", "concurrent",
-    "choking", "confidence", "chopper", "conformist", "Christmas", "congregate", "clamshell", "consensus",
-    "classic", "consulting", "classroom", "corporate", "cleanup", "corrosion", "clockwork", "councilman",
-    "cobra", "crossover", "commence", "crucifix", "concert", "cumbersome", "cowbell", "customer",
-    "crackdown", "Dakota", "cranky", "decadence", "crowfoot", "December", "crucial", "decimal",
-    "crumpled", "designing", "crusade", "detector", "cubic", "detergent", "dashboard", "determine",
-    "deadbolt", "dictator", "deckhand", "dinosaur", "dogsled", "direction", "dragnet", "disable",
-    "drainage", "disbelief", "dreadful", "disruptive", "drifter", "distortion", "dropper", "document",
-    "drumbeat", "embezzle", "drunken", "enchanting", "Dupont", "enrollment", "dwelling", "enterprise",
-    "eating", "equation", "edict", "equipment", "egghead", "escapade", "eightball", "Eskimo",
-    "endorse", "everyday", "endow", "examine", "enlist", "existence", "erase", "exodus", "escape", "fascinate",
-    "exceed", "filament", "eyeglass", "finicky", "eyetooth", "forever", "facial", "fortitude",
-    "fallout", "frequency", "flagpole", "gadgetry", "flatfoot", "Galveston", "flytrap", "getaway",
-    "fracture", "glossary", "framework", "gossamer", "freedom", "graduate", "frighten", "gravity",
-    "gazelle", "guitarist", "Geiger", "hamburger", "glitter", "Hamilton", "glucose", "handiwork",
-    "goggles", "hazardous", "goldfish", "headwaters", "gremlin", "hemisphere", "guidance", "hesitate",
-    "hamlet", "hideaway", "highchair", "holiness", "hockey", "hurricane", "indoors", "hydraulic",
-    "indulge", "impartial", "inverse", "impetus", "involve", "inception", "island", "indigo",
-    "jawbone", "inertia", "keyboard", "infancy", "kickoff", "inferno", "kiwi", "informant",
-    "klaxon", "insincere", "locale", "insurgent", "lockup", "integrate", "merit", "intention",
-    "minnow", "inventive", "miser", "Istanbul", "Mohawk", "Jamaica", "mural", "Jupiter", "music", "leprosy",
-    "necklace", "letterhead", "Neptune", "liberty", "newborn", "maritime", "nightbird", "matchmaker",
-    "Oakland", "maverick", "obtuse", "Medusa", "offload", "megaton", "optic", "microscope", "orca", "microwave",
-    "payday", "midsummer", "peachy", "millionaire", "pheasant", "miracle", "physique", "misnomer",
-    "playhouse", "molasses", "Pluto", "molecule", "preclude", "Montana", "prefer", "monument",
-    "preshrunk", "mosquito", "printer", "narrative", "prowler", "nebula", "pupil", "newsletter",
-    "puppy", "Norwegian", "python", "October", "quadrant", "Ohio", "quiver", "onlooker", "quota", "opulent",
-    "ragtime", "Orlando", "ratchet", "outfielder", "rebirth", "Pacific", "reform", "pandemic", "regain", "Pandora",
-    "reindeer", "paperweight", "rematch", "paragon", "repay", "paragraph", "retouch", "paramount",
-    "revenge", "passenger", "reward", "pedigree", "rhythm", "Pegasus", "ribcage", "penetrate",
-    "ringbolt", "perceptive", "robust", "performance", "rocker", "pharmacy", "ruffled", "phonetic",
-    "sailboat", "photograph", "sawdust", "pioneer", "scallion", "pocketful", "scenic", "politeness",
-    "scorecard", "positive", "Scotland", "potato", "seabird", "processor", "select", "provincial",
-    "sentence", "proximate", "shadow", "puberty", "shamrock", "publisher", "showgirl", "pyramid",
-    "skullcap", "quantity", "skydive", "racketeer", "slingshot", "rebellion", "slowdown", "recipe",
-    "snapline", "recover", "snapshot", "repellent", "snowcap", "replica", "snowslide", "reproduce",
-    "solo", "resistor", "southward", "responsive", "soybean", "retraction", "spaniel", "retrieval",
-    "spearhead", "retrospect", "spellbind", "revenue", "spheroid", "revival", "spigot", "revolver",
-    "spindle", "sandalwood", "spyglass", "sardonic", "stagehand", "Saturday", "stagnate", "savagery",
-    "stairway", "scavenger", "standard", "sensation", "stapler", "sociable", "steamship", "souvenir",
-    "sterling", "specialist", "stockman", "speculate", "stopwatch", "stethoscope", "stormy", "stupendous",
-    "sugar", "supportive", "surmount", "surrender", "suspense", "suspicious", "sweatband", "sympathy",
-    "swelter", "tambourine", "tactics", "telephone", "talon", "therapist", "tapeworm", "tobacco",
-    "tempest", "tolerance", "tiger", "tomorrow", "tissue", "torpedo", "tonic", "tradition", "topmost", "travesty",
-    "tracker", "trombonist", "transit", "truncated", "trauma", "typewriter", "treadmill", "ultimate",
-    "Trojan", "undaunted", "trouble", "underfoot", "tumor", "unicorn", "tunnel", "unify", "tycoon", "universe",
-    "uncut", "unravel", "unearth", "upcoming", "unwind", "vacancy", "uproot", "vagabond", "upset", "vertigo",
-    "upshot", "Virginia", "vapor", "visitor", "village", "vocalist", "virus", "voyager", "Vulcan", "warranty",
-    "waffle", "Waterloo", "wallet", "whimsical", "watchword", "Wichita", "wayside", "Wilmington",
-    "willow", "Wyoming", "woodlark", "yesteryear", "Zulu", "Yucatan"].each_slice(2) do |evn, odd|
+  %w[ aardvark adroitness absurd adviser accrue aftermath acme aggregate
+      adrift alkali adult almighty afflict amulet ahead amusement
+      aimless antenna Algol applicant allow Apollo alone armistice
+      ammo article ancient asteroid apple Atlantic artist atmosphere
+      assume autopsy Athens Babylon atlas backwater Aztec barbecue
+      baboon belowground backfield bifocals backward bodyguard banjo bookseller
+      beaming borderline bedlamp bottomless beehive Bradbury beeswax bravado
+      befriend Brazilian Belfast breakaway berserk Burlington billiard businessman
+      bison butterfat blackjack Camelot blockade candidate blowtorch cannonball
+      bluebird Capricorn bombast caravan bookshelf caretaker brackish celebrate
+      breadline cellulose breakup certify brickyard chambermaid briefcase Cherokee
+      Burbank Chicago button clergyman buzzard coherence cement combustion
+      chairlift commando chatter company checkup component chisel concurrent
+      choking confidence chopper conformist Christmas congregate clamshell consensus
+      classic consulting classroom corporate cleanup corrosion clockwork councilman
+      cobra crossover commence crucifix concert cumbersome cowbell customer
+      crackdown Dakota cranky decadence crowfoot December crucial decimal
+      crumpled designing crusade detector cubic detergent dashboard determine
+      deadbolt dictator deckhand dinosaur dogsled direction dragnet disable
+      drainage disbelief dreadful disruptive drifter distortion dropper document
+      drumbeat embezzle drunken enchanting Dupont enrollment dwelling enterprise
+      eating equation edict equipment egghead escapade eightball Eskimo
+      endorse everyday endow examine enlist existence erase exodus
+      escape fascinate exceed filament eyeglass finicky eyetooth forever
+      facial fortitude fallout frequency flagpole gadgetry flatfoot Galveston
+      flytrap getaway fracture glossary framework gossamer freedom graduate
+      frighten gravity gazelle guitarist Geiger hamburger glitter Hamilton
+      glucose handiwork goggles hazardous goldfish headwaters gremlin hemisphere
+      guidance hesitate hamlet hideaway highchair holiness hockey hurricane
+      indoors hydraulic indulge impartial inverse impetus involve inception
+      island indigo jawbone inertia keyboard infancy kickoff inferno
+      kiwi informant klaxon insincere locale insurgent lockup integrate
+      merit intention minnow inventive miser Istanbul Mohawk Jamaica
+      mural Jupiter music leprosy necklace letterhead Neptune liberty
+      newborn maritime nightbird matchmaker Oakland maverick obtuse Medusa
+      offload megaton optic microscope orca microwave payday midsummer
+      peachy millionaire pheasant miracle physique misnomer playhouse molasses
+      Pluto molecule preclude Montana prefer monument preshrunk mosquito
+      printer narrative prowler nebula pupil newsletter puppy Norwegian
+      python October quadrant Ohio quiver onlooker quota opulent
+      ragtime Orlando ratchet outfielder rebirth Pacific reform pandemic
+      regain Pandora reindeer paperweight rematch paragon repay paragraph
+      retouch paramount revenge passenger reward pedigree rhythm Pegasus
+      ribcage penetrate ringbolt perceptive robust performance rocker pharmacy
+      ruffled phonetic sailboat photograph sawdust pioneer scallion pocketful
+      scenic politeness scorecard positive Scotland potato seabird processor
+      select provincial sentence proximate shadow puberty shamrock publisher
+      showgirl pyramid skullcap quantity skydive racketeer slingshot rebellion
+      slowdown recipe snapline recover snapshot repellent snowcap replica
+      snowslide reproduce solo resistor southward responsive soybean retraction
+      spaniel retrieval spearhead retrospect spellbind revenue spheroid revival
+      spigot revolver spindle sandalwood spyglass sardonic stagehand Saturday
+      stagnate savagery stairway scavenger standard sensation stapler sociable
+      steamship souvenir sterling specialist stockman speculate stopwatch stethoscope
+      stormy stupendous sugar supportive surmount surrender suspense suspicious
+      sweatband sympathy swelter tambourine tactics telephone talon therapist
+      tapeworm tobacco tempest tolerance tiger tomorrow tissue torpedo
+      tonic tradition topmost travesty tracker trombonist transit truncated
+      trauma typewriter treadmill ultimate Trojan undaunted trouble underfoot
+      tumor unicorn tunnel unify tycoon universe uncut unravel
+      unearth upcoming unwind vacancy uproot vagabond upset vertigo
+      upshot Virginia vapor visitor village vocalist virus voyager
+      Vulcan warranty waffle Waterloo wallet whimsical watchword Wichita
+      wayside Wilmington willow Wyoming woodlark yesteryear Zulu Yucatan
+    ].each_slice(2) do |evn, odd|
     PGP_EVEN_WORDS << evn
     PGP_ODD_WORDS << odd
   end
@@ -96,12 +100,15 @@ class BaseN
   end
 
   # Converts a baseN string to a base10 integer.
-  def self.base_to_int(base, base_val)
-    base_val = base_val.split('') unless Array===base_val
+  def self.base_to_int(base, base_v)
+    base_val = Array===base_v ? base_v : base_v.split('')
     if SYMBOLS[base]
       base_len = BASES[base]
       base = SYMBOLS[base]
     elsif base.respond_to?(:length)
+      if String===base
+        base = base.split('')
+      end
       rotating_symbol_map = (Array===base[0])
       if rotating_symbol_map
         base.each{|b| ensure_uniques(b)}
@@ -118,6 +125,8 @@ class BaseN
       base_len = base.length
     end
     rotating_symbol_map = (Array===base[0])
+    base_val = base_v.split(' ') if String===base_v
+    base_val = base_val.first.split('') if base_val.length==1
     return_val = if base_len == 1
       base_val.length
     else
@@ -127,7 +136,7 @@ class BaseN
       base_val.reverse.each_with_index do |char,index|
         i = rotating_symbol_map ? index+1 : index # due to endianness issues...
         this_base = rotating_symbol_map ? base[i % base.length] : base
-        raise ArgumentError, "Value passed, '#{char}', not in specified character set '#{this_base}'#{' which is a rotating symbol map' if rotating_symbol_map}" if (char_index = this_base.index(char)).nil?
+        raise ArgumentError, "Value passed, '#{char}', not in specified symbol set '#{this_base}'#{' which is a rotating symbol map' if rotating_symbol_map}" if (char_index = this_base.index(char)).nil?
         int_val += (char_index*(base_len**index))
       end
       int_val
@@ -153,7 +162,7 @@ class BaseN
     elsif !base
       # use an implicit character set
       implicit_base = true
-      base = base_val.split('').inject({}){|h, c| h[c]=true; h}.keys
+      base = int_val.split('').inject({}){|h, c| h[c]=true; h}.keys
       base_len = base.length
     end
     raise ArgumentError, "Value passed, '#{int_val}', is not an Integer." unless int_val.is_a?(Integer)
@@ -188,6 +197,18 @@ class BaseN
     end
   end
 
+end
+
+class String
+  def decode(*args)
+    BaseN.decode(*args, self)
+  end
+end
+
+class Integer
+  def encode(*args)
+    BaseN.encode(*args, self)
+  end
 end
 
 ########## inline tests
@@ -248,6 +269,9 @@ if __FILE__==$PROGRAM_NAME
     end
     def test_bin_to_hex_to_bin
       assert_equal '5acf456e9a3d', BaseN.bin_to_hex(BaseN.hex_to_bin('5acf456e9a3d'))
+    end
+    def test_core_monkeypatch_mode
+      assert_equal "aftermath hamlet graduate", "raybeam".decode(' raybem').encode(:pgp_words)
     end
   end
 end
